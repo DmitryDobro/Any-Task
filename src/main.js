9e2ce96d8c0a4311dfdfb6b4b8e585bb9c3642a6
Dima Dobronovich <dobronovich_95@mail.ru>
2025-06-14 16:51:40 +0700

    init

--- src/main.js
+++ src/main.js
@@ -1,13 +1,1 @@
-import './style.scss';
 
-let catArr = document.querySelectorAll('.category__name');
-
-for (let i = 0; catArr.length > i; i++) {
-  catArr[i].addEventListener('click', evt => {
-    console.log(catArr[i].parentNode.querySelector('.subcategory__list'));
-
-    let subCat = catArr[i].parentNode.querySelector('.subcategory__list');
-    subCat.classList.toggle('subcategory_type_active');
-    catArr[i].classList.toggle('category_type_active');
-  });
-}
