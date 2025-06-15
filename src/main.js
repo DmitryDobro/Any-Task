d3b9f39a8b9904af397e4fe617c8e6104e072333
Dima Dobronovich <dobronovich_95@mail.ru>
2025-06-15 13:10:32 +0700

    feat/реализованы функции

--- src/main.js
+++ src/main.js
@@ -1,1 +1,34 @@
+import './style.scss';
 
+const arr = document.querySelectorAll('.card');
+let arrAtr = [];
+arr.forEach(item => {
+  arrAtr.push(item.getAttribute('data-product-id'));
+});
+
+function addSymbol(arr) {
+  return arr.map(item => {
+    let result = '';
+    item = '0' + item;
+    for (let i = 0; i < item.length; i++) {
+      if (i % 3 == 0 && i !== 0) {
+        result += '-' + item[i];
+      } else {
+        result += item[i];
+      }
+    }
+    return result;
+  });
+}
+let a = addSymbol(arrAtr);
+
+// ==============
+
+const list = document.querySelectorAll('.li');
+let obj = {};
+list.forEach(item => {
+  let attr = item.querySelector('.parameter-name').childNodes[0].textContent.trim();
+  let value = item.querySelector('.parameter-value').childNodes[0].textContent.trim();
+  obj[attr] = value;
+});
+console.log(obj);
