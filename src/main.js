08bb2380676f33423e30229fbfc0fc5f813b6143
Dima Dobronovich <dobronovich_95@mail.ru>
2025-06-16 19:10:50 +0700

    fix/финальный рефакторинг, немного исправил стили, дабовил readme файл

--- src/main.js
+++ src/main.js
@@ -1,24 +1,4 @@
 import './style.scss';
 import './scripts/clearCheckboxes';
 import './scripts/dropDownList';
-
-let input = document.getElementById('headerInput');
-let btn = document.querySelector('.header__input-btn');
-let icon = document.getElementById('headerClearIcon');
-
-input.addEventListener('input', evt => {
-  console.log(icon);
-
-  if (input.value) {
-    btn.style.display = 'block';
-    icon.style.display = 'block';
-  } else {
-    btn.style.display = 'none';
-    icon.style.display = 'none';
-  }
-});
-icon.addEventListener('click', () => {
-  input.value = '';
-  btn.style.display = 'none';
-  icon.style.display = 'none';
-});
+import './scripts/showElements';
---
+++ src/scripts/showElements.js
@@ -0,0 +1,19 @@
+let input = document.getElementById('headerInput');
+let btn = document.querySelector('.header__input-btn');
+let icon = document.getElementById('headerClearIcon');
+
+input.addEventListener('input', evt => {
+  console.log(icon);
+  if (input.value) {
+    btn.style.display = 'block';
+    icon.style.display = 'block';
+  } else {
+    btn.style.display = 'none';
+    icon.style.display = 'none';
+  }
+});
+icon.addEventListener('click', () => {
+  input.value = '';
+  btn.style.display = 'none';
+  icon.style.display = 'none';
+});
